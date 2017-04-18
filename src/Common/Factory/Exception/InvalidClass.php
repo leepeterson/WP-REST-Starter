7d9f0378172a432e34fc2cfe38c45057341dd3ba
tfrommen <info@tfrommen.de>
2017-04-18 10:00:20 +0200

    Fix typo.

--- src/Common/Factory/Exception/InvalidClass.php
+++ src/Common/Factory/Exception/InvalidClass.php
@@ -7,7 +7,7 @@ namespace Inpsyde\WPRESTStarter\Common\Factory\Exception;
 /**
  * Exception to be thrown when trying to create an instance of an invalid class.
  *
- * @package Inpsyde\WPRESTStarter\Common\Facrtory\Exception
+ * @package Inpsyde\WPRESTStarter\Common\Factory\Exception
  * @since   3.0.0
  */
 class InvalidClass extends \Exception {
