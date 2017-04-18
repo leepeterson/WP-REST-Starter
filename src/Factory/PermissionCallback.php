c8aacb490eaf24f2c772fef436f662efa098722d
tfrommen <info@tfrommen.de>
2017-04-18 10:44:55 +0200

    Introduce current_user_can_for_site() permission callback factory method.

--- src/Factory/PermissionCallback.php
+++ src/Factory/PermissionCallback.php
@@ -42,4 +42,41 @@ class PermissionCallback {
 			return true;
 		};
 	}
+
+	/**
+	 * Returns a callback that checks if the current user has all of the given capabilities in the site with the given
+	 * ID.
+	 *
+	 * @since 3.0.0
+	 *
+	 * @param int      $site_id         Site ID.
+	 * @param string[] ...$capabilities Capabilities required to get permission.
+	 *
+	 * @return \Closure Callback that checks if the current user has all of the given capabilities.
+	 */
+	public function current_user_can_for_site( int $site_id, string ...$capabilities ): \Closure {
+
+		if ( ! \is_multisite() ) {
+			// Not a multisite. Check the current site regardless of the given site ID.
+			return $this->current_user_can( ...$capabilities );
+		}
+
+		/**
+		 * Checks if the current user has specific capabilities.
+		 *
+		 * @since 3.0.0
+		 *
+		 * @return bool Whether or not the current user has specific capabilities.
+		 */
+		return function () use ( $site_id, $capabilities ) {
+
+			\switch_to_blog( $site_id );
+
+			$current_user_can = $this->current_user_can( ...$capabilities )();
+
+			\restore_current_blog();
+
+			return $current_user_can;
+		};
+	}
 }
