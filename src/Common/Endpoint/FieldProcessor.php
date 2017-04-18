7df0e6c8a6b7b3802c841d96ef44f04dc271657b
tfrommen <info@tfrommen.de>
2017-04-18 10:26:17 +0200

    Rename method.

--- src/Common/Endpoint/FieldProcessor.php
+++ src/Common/Endpoint/FieldProcessor.php
@@ -7,18 +7,19 @@ namespace Inpsyde\WPRESTStarter\Common\Endpoint;
  *
  * @package Inpsyde\WPRESTStarter\Common\Endpoint
  * @since   2.0.0
+ * @since   3.0.0 Renamed "get_extended_properties()" to "add_fields_to_properties()".
  */
 interface FieldProcessor {
 
 	/**
 	 * Returns the given properties with added data of all schema-aware fields registered for the given object type.
 	 *
-	 * @since 2.0.0
+	 * @since 3.0.0
 	 *
 	 * @param array  $properties  Schema properties definition.
 	 * @param string $object_type Object type.
 	 *
 	 * @return array Properties with added data of all schema-aware fields registered for the given object type.
 	 */
-	public function get_extended_properties( array $properties, string $object_type ): array;
+	public function add_fields_to_properties( array $properties, string $object_type ): array;
 }
